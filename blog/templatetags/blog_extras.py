828b8c7a37a298b0920ef0cd05fc2b1a8451d1eb
Aayush Jain <aayushjn11@gmail.com>
2023-01-02 05:39:31 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django import template
+from blog.models import Post
 
 user_model = get_user_model()
 register = template.Library()
@@ -23,3 +24,28 @@ def author_details(author, current_user=None):
     return format_html('<a href="mailto:{}">{}</a>', author.email, name)
   else:
     return format_html("{}", name)
+
+
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
+
+
+@register.simple_tag
+def endrow():
+  return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+  return format_html("</div>")
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+  posts = Post.objects.exclude(pk=post.pk).order_by("published_at")[:5]
+  return {"title": "Recent Posts", "posts": posts}
